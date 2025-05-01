c8fa928085ec91c56bd24845990d6a7aba689fce
ahmed ahmed <engineerahmed413@gmail.com>
2025-05-01 18:30:53 +0300

    .

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.all('*', (req, res, next) => {
 // Global error handling middleware for express
 app.use(globalError);
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`App running running on port ${PORT}`);
 });
